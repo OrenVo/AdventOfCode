3f59cfae90917bfac24dbd6b595f91a47f839a44
Vojtěch Ulej <ulej.vojta@gmail.com>
2024-12-03 17:47:18 +0100

    refactor day 2

--- AoC2024/02/main.py
+++ AoC2024/02/main.py
@@ -26,23 +26,6 @@ def check_safety_with_dampener(l: list):
 
 a = []
 
-total = 0
-total2 = 0
-
-
-def check(a):
-    diff = [a[i + 1] - a[i] for i in range(len(a) - 1)]
-    if all(d > 0 and d <= 3 for d in diff) or all(d < 0 and d >= -3 for d in diff):
-        return True
-    return False
-
-
-def checkrem(a):
-    for i in range(len(a)):
-        if check(a[:i] + a[i + 1 :]):
-            return True
-    return False
-
 
 with open("00input", "r") as f:
     a = [[int(x) for x in l.split(" ")] for l in f.readlines()]
@@ -54,9 +37,3 @@ not_safe = [x for x in a if not check_safety(x)]
 print(len(safe))
 
 print(len(safe) + len([x for x in not_safe if check_safety_with_dampener(x)]))
-
-for i in a:
-    if check(i):
-        total += 1
-    elif checkrem(i):
-        total2 += 1
